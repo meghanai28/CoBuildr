5bfcce7f05331c4324f901ca968469557970672a
katiethng <143026594+katiethng@users.noreply.github.com>
2024-05-09 01:08:16 -0700

    advisorUI updated

--- CoBuildr/lib/routes/advisor/advisor_chat.dart
+++ CoBuildr/lib/routes/advisor/advisor_chat.dart
@@ -5,23 +5,35 @@ import 'package:namer_app/routes/chat/chat_details.dart';
 import 'package:namer_app/services/chat_service.dart';
 import 'package:intl/intl.dart';
 
+
 class AdvisorChatPage extends StatefulWidget {
   const AdvisorChatPage({Key? key}) : super(key: key);
 
+
   @override
-  State<AdvisorChatPage> createState() => _ChatPageState(); // create the state
+  State<AdvisorChatPage> createState() => _AdvisorChatPageState(); // create the state
 }
 
-class _ChatPageState extends State<AdvisorChatPage> {
+
+class _AdvisorChatPageState extends State<AdvisorChatPage> {
   final FirebaseAuth _auth = FirebaseAuth.instance; // create instance of authentication to be used to get current user
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false, // get rid of back button for now (so buggy)
-        title: const Text('Chat Page'), // name of the page
+        title: Center(
+          child: Text(
+            'Messages',
+            style: TextStyle(
+              color: const Color.fromARGB(255, 111, 15, 128),
+            ),
+          ),
+        ), // name of the page
       ),
+
 
       body: Column( // show the list of the users that the current user has messages with
         children: [
@@ -31,12 +43,12 @@ class _ChatPageState extends State<AdvisorChatPage> {
         ],
       ),
 
+
       floatingActionButton: FloatingActionButton(
-        backgroundColor: Color.fromARGB(255, 219, 110, 255),
+        backgroundColor: Color.fromARGB(255, 111, 15, 128),
         onPressed: () {
           _createNewChatDialog(context);
         },
-        mini: true, // mini smaller
         child: Icon(Icons.add, color: Colors.white),
       ),
       bottomNavigationBar: BottomNavigationBar(
@@ -47,15 +59,16 @@ class _ChatPageState extends State<AdvisorChatPage> {
             // Navigate to Dashboard page
             Navigator.pushNamed(context, '/advisor/advisor_dashboard');
           } else if (index == 1) {
-            // Navigate to Project Tab
+            // Navigate to Your Projects page
             Navigator.pushNamed(context, '/advisor/project_tab');
           } else if (index == 3) {
+            // Navigate to Settings page
             Navigator.pushNamed(context, '/advisor/advisor_setting');
           }
         },
         items: [
           _buildNavItem(Icons.dashboard, 'Dashboard'),
-          _buildNavItem(Icons.list, 'Your Projects'),
+          _buildNavItem(Icons.list, 'Projects'),
           _buildNavItem(Icons.message, 'Messages'),
           _buildNavItem(Icons.settings, 'Settings'),
         ],
@@ -64,14 +77,15 @@ class _ChatPageState extends State<AdvisorChatPage> {
     );
   }
 
-      // build list of users that user is currently chatting with
+
+  // build list of users that user is currently chatting with
   Widget _buildUserList() {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16.0), // Add horizontal padding
       child : Container( // create container widget
         padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0), // padding
         decoration: BoxDecoration( // color decoration
-          color: Colors.purple[50], // light purple 
+          color: Colors.purple[50], // light purple
           borderRadius: BorderRadius.circular(8.0), // more circular cornoers
         ),
         child: Column(
@@ -88,7 +102,7 @@ class _ChatPageState extends State<AdvisorChatPage> {
                 ),
               ),
             ),
-          
+         
             Expanded(
               child: ScrollbarTheme(
                 data: ScrollbarThemeData(
@@ -104,10 +118,11 @@ class _ChatPageState extends State<AdvisorChatPage> {
                       return const Text(''); // show loading if the data is still being loaded in
                     }
 
+
                     final chatRooms = snapshot.data!.docs; // get the users
-        
+       
                     return ListView.builder( // build a list of all users (like how we see in imessages)
-                    
+                   
                       padding: EdgeInsets.only(right: 16.0),
                       itemCount: chatRooms.length,
                       itemBuilder: (context, index) {
@@ -121,9 +136,9 @@ class _ChatPageState extends State<AdvisorChatPage> {
                              future: _getUserEmailByUid(otherUserId),
                              builder: (context, snapshot) {
                               if (snapshot.connectionState == ConnectionState.waiting) {
-                                return Text(''); 
+                                return Text('');
                               } else if (snapshot.hasError) {
-                                return Text('Error: ${snapshot.error}'); 
+                                return Text('Error: ${snapshot.error}');
                               }
                               final email = snapshot.data.toString();
                               return Material( // material so the flutter animation when hover shows up
@@ -132,7 +147,7 @@ class _ChatPageState extends State<AdvisorChatPage> {
                                   children: [
                                     ListTile(  
                                       title: Text(email),
-                                      subtitle: Text(formattedDate, style: 
+                                      subtitle: Text(formattedDate, style:
                                         TextStyle(
                                           fontSize: 10
                                         ),
@@ -152,19 +167,19 @@ class _ChatPageState extends State<AdvisorChatPage> {
                                           Icons.arrow_forward, // arrow icon
                                           color: Colors.purple,
                                         ),
-                                        contentPadding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0), // add more padding 
+                                        contentPadding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0), // add more padding
                                       ),
-                                      Divider( // add a divider 
+                                      Divider( // add a divider
                                         color: Colors.white,
                                         thickness: 1.0,
                                         height: 0.0,
                                       ),
                                     ]
                                   )
-                                ); 
+                                );
                              }
                           );
-                        } 
+                        }
                         else { // if the user is the current user than just return empty (we dont want to message ourselves)
                           // empty
                           return Container();
@@ -178,8 +193,9 @@ class _ChatPageState extends State<AdvisorChatPage> {
           ]
         )
       )
-    );     
+    );    
   }
+
 
   // popup dialog for creating a new chat
   void _createNewChatDialog(BuildContext context) {
@@ -187,8 +203,9 @@ class _ChatPageState extends State<AdvisorChatPage> {
     TextEditingController _messageController = TextEditingController(); // give a place for message to be also used
     ChatService _chatService = ChatService(); // use chat services since we are sending a message
 
+
     showDialog( // show Dialog method helps us do a popup on creen
-      context: context, 
+      context: context,
       builder: (BuildContext context) {
         return AlertDialog( // use alert dialog to get it one screen w the needed buttons and actions
           title: Text("Create Chat Message"), // name of the popup
@@ -203,9 +220,9 @@ class _ChatPageState extends State<AdvisorChatPage> {
                       labelText: 'User Email', // tell them to input the user email
                   ),
                 ),
-          
+         
               ),
-              
+             
               Expanded(
                 child: TextField(
                   controller: _messageController, // let user input their message
@@ -214,18 +231,18 @@ class _ChatPageState extends State<AdvisorChatPage> {
                       labelText: 'Message', // tell them this is where they input the user message
                   ),
                 ),
-          
+         
               ),
-  
+ 
             ]
           ),
-          
+         
           actions: <Widget>[
             TextButton( // button where user can exit the widget
               onPressed: () => Navigator.pop(context),
               child: Text("Cancel"),
             ),
-        
+       
             TextButton( // button where user can send their message
               onPressed: () async { // cause we call an async method
                 if (_messageController.text.isEmpty || _emailController.text.isEmpty) // before anything check if user is trying to send empty chat
@@ -244,7 +261,7 @@ class _ChatPageState extends State<AdvisorChatPage> {
                       Navigator.of(context).pop();
                       Navigator.pushNamed(context, '/chat');
                   }
-                    
+                   
                   else { // if it doesnt exist
                     ScaffoldMessenger.of(context).showSnackBar(
                       SnackBar(
@@ -253,15 +270,16 @@ class _ChatPageState extends State<AdvisorChatPage> {
                     );
                   }
                 }
-                
+               
               },
               child: Text("Send"), // this is the send button which does all of the stuff above when pressed
             ),
           ],
-        );   
+        );  
       },
     );
   }
+
 
   // given email get uid
   Future<String?> _getUserUidByEmail(String email) async {
@@ -269,9 +287,10 @@ class _ChatPageState extends State<AdvisorChatPage> {
         .collection('users')
         .where('email', isEqualTo: email)
         .get();
-    
+   
     return querySnapshot.docs.isNotEmpty ? querySnapshot.docs.first.id : null; // check if the query exists, if it doesnt return null if it does return the query snapshot of the first id
   }
+
 
   // given uid get email
   Future<String?> _getUserEmailByUid(String id) async {
@@ -279,9 +298,10 @@ class _ChatPageState extends State<AdvisorChatPage> {
         .collection('users')
         .where('uid', isEqualTo: id)
         .get();
-    
+   
     return querySnapshot.docs.isNotEmpty ? querySnapshot.docs.first.get('email') : null; // check if the query exists, if it doesnt return null if it does return the query snapshot of the first id
   }
+
 
   BottomNavigationBarItem _buildNavItem(IconData icon, String label) {
     return BottomNavigationBarItem(
